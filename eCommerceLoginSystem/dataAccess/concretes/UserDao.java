e3e4e23678803583e0248081ff62a66a0f0355f3
galiptasli <59477693+galiptasli@users.noreply.github.com>
2021-05-08 15:17:16 +0300

    Create UserDao.java

---
+++ eCommerceLoginSystem/dataAccess/concretes/UserDao.java
@@ -0,0 +1,32 @@
+package eCommerceLoginSystem.dataAccess.concretes;
+
+import eCommerceLoginSystem.dataAccess.abstracts.UserDaoService;
+import eCommerceLoginSystem.entities.concretes.User;
+
+public class UserDao implements UserDaoService {
+
+	@Override
+	public void add(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public User getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
